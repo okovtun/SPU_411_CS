43a1a4f5923cf5224ae53c028f2036f66bf506b4
User <User@DESKTOP-QHG18FL>
2025-08-30 13:35:35 +0300

    Point

---
+++ OOP/OOP/Point.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+	class Point
+	{
+		//Автоствойства:
+		public double X { get; set; }
+		public double Y { get; set; }
+		//double x;
+		//double y;
+		////internal, protected internal
+		////Properties:
+		//public double X
+		//{
+		//	get { return x; }
+		//	set { x = value;}
+		//}
+		//public double Y
+		//{
+		//	get { return y; }
+		//	set { y = value; }
+		//}
+
+		////Get/Set:
+		//public double GetX()
+		//{
+		//	return x;
+		//}
+		//public double GetY()
+		//{
+		//	return y;
+		//}
+		//public void SetX(double x)
+		//{
+		//	this.x = x;
+		//}
+		//public void SetY(double y)
+		//{
+		//	this.y = y;
+		//}
+
+		//			Methods:
+		public void Print()
+		{
+			Console.WriteLine($"X = {X}, Y = {Y}");
+			//Console.WriteLine($"X = {GetX()}, Y = {GetY()}");
+		}
+	}
+}
---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Point point = new Point();
+			//point.SetX(2);
+			//point.SetY(3);
+			//point.Print();
+
+			point.X = 7;
+			point.Y = 8;
+			Console.WriteLine($"Properties: X = {point.X},Y = {point.Y}");
+		}
+	}
+}
